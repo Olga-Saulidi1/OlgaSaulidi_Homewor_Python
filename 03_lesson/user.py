9103fd92b597633889db85e05c28249614910837
Olga-Saulidi1 <saulidi.olga@yandex.ru>
2026-01-07 14:08:53 +0300

    Корректировка задания 1 урока №3

--- 03_lesson/user.py
+++ 03_lesson/user.py
@@ -1,5 +1,5 @@
 class User:
-    def __init__(self, first_name, last_name, phone_number):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
@@ -8,6 +8,6 @@ class User:
     
     def get_last_name (self):
         return self.last_name
-    
+     
     def get_product_info (self):
-        return f"имя: {self.first_name}, фамилия: {self.last_name}"    
+        return f"{self.first_name} {self.last_name}"    
